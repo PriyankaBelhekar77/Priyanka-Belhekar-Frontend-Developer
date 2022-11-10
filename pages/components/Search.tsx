77a8c4bb708298010d3644675f0eef10effcfce8
Priyanka Belhekar <priyankaBelhekar77@gmail.com>
2022-11-11 05:04:54 +0530

    feat(search): added search functionality

--- pages/components/Search.tsx
+++ pages/components/Search.tsx
@@ -1,6 +1,16 @@
-const Search = () => {
+import { ChangeEventHandler } from "react";
+
+const Search = ({ capsules, setState }: any) => {
+  const onChange = (e: any) => {
+    console.log(e.target.value)
+    setState(prevState => {
+      const newState = capsules.filter(capsule => capsule.capsule_serial.includes(e.target.value));
+      console.log(newState)
+      return newState;
+    })
+  }
   return (
-    <input placeholder="enter name" />
+    <input placeholder="enter name" onChange={onChange} />
   )
 }
 
