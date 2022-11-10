f639693977ae9a11262aa48946f507feb68d7f08
Priyanka Belhekar <priyankaBelhekar77@gmail.com>
2022-11-11 02:59:56 +0530

    feat(modal): Added more details in modal

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -9,7 +9,14 @@ const Card = (props: Capsule) => {
     <div className="relative w-40 h-40 bg-slate-400 cursor-pointer text-center rounded" onClick={() => setIsOpen(true)}>
       {props.capsule_serial}
     </div>
-    {isOpen && <Modal onClose={() => setIsOpen(false)} />}
+    {isOpen && <Modal 
+      onClose={() => setIsOpen(false)} 
+      content={<div className="px-2">
+        <div><span>{props.capsule_serial}</span></div>
+        <div><span>{props.details}</span></div>
+        <div><span>{props.landings}</span></div>
+      </div>}
+      />}
     </>
   )
 }
--- pages/components/Modal.tsx
+++ pages/components/Modal.tsx
@@ -1,13 +1,20 @@
-const Modal = (props: any) => {
+
+interface ModalProps {
+  onClose: () => void;
+  content: React.ReactElement;
+}
+
+const Modal = ({ onClose, content }: ModalProps) => {
   return (
     // modal overlay
-    <div className="fixed top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,0.6)] flex justify-center items-center" onClick={props.onClose}>
+    <div className="fixed top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,0.6)] flex justify-center items-center" onClick={onClose}>
       {/** Modal container */}
       <div className="w-40 h-40 bg-white inline-block rounded">
          {/** Modal Header */}
         <div>
         </div>
         {/** Modal Content */}
+          {content}
         <div>
         </div>
       </div>
