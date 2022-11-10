50a351434f77f9222a61a56b714ec05fbc949343
Priyanka Belhekar <priyankaBelhekar77@gmail.com>
2022-11-11 05:06:43 +0530

    fix(build): fixing the build

--- pages/components/Search.tsx
+++ pages/components/Search.tsx
@@ -3,8 +3,8 @@ import { ChangeEventHandler } from "react";
 const Search = ({ capsules, setState }: any) => {
   const onChange = (e: any) => {
     console.log(e.target.value)
-    setState(prevState => {
-      const newState = capsules.filter(capsule => capsule.capsule_serial.includes(e.target.value));
+    setState(() => {
+      const newState = capsules.filter((capsule: any) => capsule.capsule_serial.includes(e.target.value));
       console.log(newState)
       return newState;
     })
