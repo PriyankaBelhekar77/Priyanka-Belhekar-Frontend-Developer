4e6626af6c2b4a2bb76409bd18e11ddbaa684bcf
Priyanka Belhekar <priyankaBelhekar77@gmail.com>
2022-11-12 03:32:39 +0530

    feat(modal): added icons

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -1,4 +1,10 @@
 import { useState } from "react";
+import { GrLaunch } from 'react-icons/gr'
+import { BsArrowCounterclockwise } from 'react-icons/bs'
+import { BiDetail } from 'react-icons/bi';
+import { TbHelicopterLanding, TbListDetails } from 'react-icons/tb';
+import { DiLinux } from 'react-icons/di';
+import { FaDragon } from 'react-icons/fa';
 import type { Capsule, Capsules } from "..";
 import Modal from "./Modal";
 
@@ -7,6 +13,21 @@ const STATUS = {
   retired: 'text-blue-600',
   unknown: 'text-red-600',
   active: 'text-green-600'
+}
+
+interface RowProps {
+  icon: React.ReactElement;
+  row: string;
+  value: string | number;
+}
+
+const Row = ({ icon, row, value }: RowProps) => {
+  return (
+    <div className="flex gap-1">
+      <div className="flex items-center gap-1 ">{icon} {row}</div> 
+      <span className="font-semibold">{value}</span>
+    </div>
+  )
 }
 
 const Card = (props: Capsule) => {
@@ -23,13 +44,14 @@ const Card = (props: Capsule) => {
       onClose={() => setIsOpen(false)}
       header={<h3 className="text-xl">Additional Details</h3>} 
       content={<div className="px-2">
-        <div>Type <span className="font-semibold">{props.type}</span></div>
-        <div>Launch Date <span className="font-semibold">{date.toDateString()}</span></div>
-        <div>Details <span className="font-semibold">{props.details ?? 'not available'}</span></div>
-        <div>Landings <span className="font-semibold">{props.landings}</span></div>
-        <div>Launch Unix <span className="font-semibold">{props.original_launch_unix ?? 'not available'}</span></div>
-        <div>Reuse Count <span className="font-semibold">{props.reuse_count}</span></div>
-        <div>Mission Details {props.missions.length ? props.missions.map((mission, index) => <div className="pl-4" key={mission.flight}>
+        <Row icon={<FaDragon />} row="Type" value={props.type} />
+        <Row icon={<GrLaunch />} row="Launch Date" value={date.toDateString()}/>
+        <Row icon={<BiDetail />} row="Details" value={props.details ?? 'not available'} />
+        <Row icon={<TbHelicopterLanding />} row="Landings" value={props.landings} />
+        <Row icon={<DiLinux />} row="Launch Unix" value={props.original_launch_unix ?? 'not available'}/>
+        <Row icon={<BsArrowCounterclockwise />} row="Reuse Count" value={props.reuse_count}/>
+        <div> 
+          <div className="flex items-center gap-1"><TbListDetails />Mission Details </div> {props.missions.length ? props.missions.map((mission, index) => <div className="pl-4" key={mission.flight}>
           <div>- Name <span className="font-semibold">{mission.name}</span></div>
           <div className="pl-3">Flight <span className="font-semibold">{mission.flight}</span></div>
         </div>) : <span className="font-semibold">not available</span>}
--- pages/components/Modal.tsx
+++ pages/components/Modal.tsx
@@ -1,3 +1,4 @@
+import { IoIosClose } from 'react-icons/io';
 
 interface ModalProps {
   onClose: () => void;
@@ -12,9 +13,9 @@ const Modal = ({ onClose, content, header }: ModalProps) => {
     <div className="fixed top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,0.6)]" onClick={onClose}>
       </div>
       {/** Modal container */}
-      <div className="absolute w-72 h-max z-20 bg-white inline-block rounded">
+      <div className="absolute w-80 h-max z-20 bg-white inline-block rounded">
         <div className="absolute right-2 top-1 cursor-pointer hover:opacity-50">
-          <button onClick={onClose}>X</button>
+          <button onClick={onClose}><IoIosClose size={'2em'} /></button>
         </div>
          {/** Modal Header */}
         <div className="py-1 px-2">
