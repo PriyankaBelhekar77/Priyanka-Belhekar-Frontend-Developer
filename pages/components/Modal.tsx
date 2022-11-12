39d19c04d4986a2a1a6bfb6ffa75aedd2af84257
Priyanka Belhekar <priyankaBelhekar77@gmail.com>
2022-11-12 23:18:32 +0530

    fix(modal): modal is now centered

--- pages/components/Modal.tsx
+++ pages/components/Modal.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useRef } from 'react';
 import { IoIosClose } from 'react-icons/io';
+import useDisableScroll from './hooks/useDisableScroll';
 
 interface ModalProps {
   onClose: () => void;
@@ -8,18 +9,14 @@ interface ModalProps {
 }
 
 const Modal = ({ onClose, content, header }: ModalProps) => {
-  useEffect(() => {
-    const stopScroll = () => window.scrollTo(0, 0)
-    window.addEventListener('scroll', stopScroll);
-    return () => window.removeEventListener('scroll', stopScroll);
-  }, [])
+  useDisableScroll();
   return (
     <>
     {/** modal overlay */}
-    <div className="fixed top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,0.6)]" onClick={onClose}>
+    <div className="overflow-hidden flex justify-center fixed top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,0.6)]" onClick={onClose}>
       </div>
       {/** Modal container */}
-      <div className="w-screen h-screen fixed top-0 md:w-80 md:h-max z-20 bg-white inline-block rounded">
+      <div className="w-screen h-screen fixed top-[50%] md:w-80 md:h-max z-20 bg-white inline-block rounded left-[50%] -translate-x-[50%] -translate-y-[50%]">
         <div className="absolute right-2 top-1 cursor-pointer hover:opacity-50">
           <button onClick={onClose}><IoIosClose size={'2em'} /></button>
         </div>
